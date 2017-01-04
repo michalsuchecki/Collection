333ceed70e6c3a412d2cc7917ea56acc251a3fc2
Haru <michal_suchecki@outlook.com>
2017-01-04 16:50:22 +0100

    added setting.json + entity dependencies

--- src/MyInflatables/Startup.cs
+++ src/MyInflatables/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -13,8 +14,20 @@ namespace MyInflatables
 {
     public class Startup
     {
+        public IConfigurationRoot _Configuration { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath).
+                AddJsonFile("settings.json", optional: true, reloadOnChange: true).
+                AddEnvironmentVariables();
+            _Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
 
         }
