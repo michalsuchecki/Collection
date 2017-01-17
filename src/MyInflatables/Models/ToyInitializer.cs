1b3e7af563c608441b56a225cfe140dcd3355e8b
Haru <michal_suchecki@outlook.com>
2017-01-17 17:36:41 +0100

    added new sorted categories

--- src/MyInflatables/Models/ToyInitializer.cs
+++ src/MyInflatables/Models/ToyInitializer.cs
@@ -18,11 +18,18 @@ namespace MyInflatables.Models
                 var categories = new Category[]
                 {
                     new Category() { Name = "Air mattress" },
+                    new Category() { Name = "Beach balls" },
+                    new Category() { Name = "Animals" },
+                    new Category() { Name = "Islands" },
                     new Category() { Name = "Furniture" },
                     new Category() { Name = "Rafts" },
                     new Category() { Name = "Ride-on" },
                     new Category() { Name = "Tubes" },
-                };
+                    new Category() { Name = "Clothes" },
+                    new Category() { Name = "Pools" },
+                    new Category() { Name = "Other" },
+                }
+                .OrderBy(s => s.Name).ToArray();
 
                 foreach(var cat in categories)
                 {
