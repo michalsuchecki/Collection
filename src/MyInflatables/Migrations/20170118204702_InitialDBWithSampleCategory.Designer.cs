7e95119b86a4f5e2dc6ece21def38d628387bc15
Haru <michal_suchecki@outlook.com>
2017-01-18 21:47:56 +0100

    Added migrations

---
+++ src/MyInflatables/Migrations/20170118204702_InitialDBWithSampleCategory.Designer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MyInflatables.Models;
+
+namespace MyInflatables.Migrations
+{
+    [DbContext(typeof(ToyContext))]
+    [Migration("20170118204702_InitialDBWithSampleCategory")]
+    partial class InitialDBWithSampleCategory
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MyInflatables.Models.Category", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("CategoryId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("MyInflatables.Models.Category", b =>
+                {
+                    b.HasOne("MyInflatables.Models.Category")
+                        .WithMany("Parent")
+                        .HasForeignKey("CategoryId");
+                });
+        }
+    }
+}
