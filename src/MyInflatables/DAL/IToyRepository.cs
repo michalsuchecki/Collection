8e44b6b336d74200afab9f14d305b75b2b25ecc5
Haru <michal_suchecki@outlook.com>
2017-01-20 22:51:12 +0100

    Created Toys sample repository

---
+++ src/MyInflatables/DAL/IToyRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyInflatables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyInflatables.DAL
+{
+    public interface IToyRepository : IDisposable
+    {
+        IEnumerable<Toy> GetToys();
+        Toy GetToyByID(int toyId);
+        void InsertToy(Toy toy);
+        void DeleteToy(int toyId);
+        void UpdateToy(Toy toy);
+        void Save();
+    }
+}
---
+++ src/MyInflatables/DAL/ToyRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyInflatables.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyInflatables.DAL
+{
+    public class ToyRepository : IToyRepository, IDisposable
+    {
+        private ToyContext _context;
+
+        public ToyRepository(ToyContext context)
+        {
+            _context = context;
+        }
+
+        public Toy GetToyByID(int toyId)
+        {
+            return _context.Toys.Find(toyId);
+            // return _context.Toys.Where(s => s.ToyID == toyId).SingleOrDefault();
+        }
+
+        public IEnumerable<Toy> GetToys()
+        {
+            return _context.Toys.ToList();
+        }
+
+        public void DeleteToy(int toyId)
+        {
+            Toy toy = _context.Toys.Find(toyId);
+            _context.Toys.Remove(toy);
+        }
+
+        public void InsertToy(Toy toy)
+        {
+            _context.Toys.Add(toy);
+        }
+
+        public void UpdateToy(Toy toy)
+        {
+            _context.Entry(toy).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
