807da515a5890011bc887820282d0b97b5c118ec
Haru <michal_suchecki@outlook.com>
2017-01-23 23:05:40 +0100

    Database update - added index for toys

---
+++ src/MyInflatables/Migrations/20170123220033_addedProducerIndexForToys.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyInflatables.Migrations
+{
+    public partial class addedProducerIndexForToys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Index",
+                table: "Toys",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "Toys");
+        }
+    }
+}
