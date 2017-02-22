68cbe709cbb5fffd8c526d439a0390690e03b7a5
Haru <michal_suchecki@outlook.com>
2017-02-22 21:38:16 +0100

    Home controller webapi -> mvc

--- src/MyInflatables/Controllers/HomeController.cs
+++ src/MyInflatables/Controllers/HomeController.cs
@@ -8,39 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyInflatables.Controllers
 {
-    [Route("api/[controller]")]
     public class HomeController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Index()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return View();
         }
     }
 }
