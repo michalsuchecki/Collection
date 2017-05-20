7b049f2ca6565ab0c52e6af58fd8fecd5edcb120
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-20 13:05:45 +0200

    onion architecture init + domain classes

---
+++ Collection.Core/Domain/Producer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Collection.Core
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string URL { get; set; }
+    }
+}
