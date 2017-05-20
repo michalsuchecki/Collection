2070a441c01234266340a26c0029697ee4b71390
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-20 13:05:45 +0200

    onion architecture init + domain classes

---
+++ Collection.Core/Domain/Toy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Collection.Core
+{
+    public class Toy
+    {
+        [Required]
+        public int ToyID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(16)]
+        public string Index { get; set; }
+        public Category Category { get; set; }
+        public Producer Producer { get; set; }
+        public bool InCollection { get; set; }
+        public ICollection<Gallery> Gallery { get; set; }
+    }
+}
