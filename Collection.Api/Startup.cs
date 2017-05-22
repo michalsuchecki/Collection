3442ded076ea37ef32955cd1e41f51f66edf9720
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-22 14:31:49 +0200

    added autofac, added example command pattern code

---
+++ Collection.Api/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Collection.Infrastructure.IoC;
+
+namespace Collection.Api
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+            builder.RegisterModule(new ContainerModule(Configuration));
+
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+                    ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+            appLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
+        }
+    }
+}
---
+++ Collection.Infrastructure/IoC/ContainerModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Collection.Infrastructure.IoC.Modules;
+using Autofac;
+
+namespace Collection.Infrastructure.IoC
+{
+    public class ContainerModule : Module
+    {
+        private readonly IConfiguration _configuration;
+  
+        public ContainerModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Register Modules
+            builder.RegisterModule<CommandModule>();
+            builder.RegisterModule<ServiceModule>();
+
+        }
+    }
+}
