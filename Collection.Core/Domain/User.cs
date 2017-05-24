a280b7d6dce9ce3b0859bde8cd5b5c73a74f7235
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-24 21:13:31 +0200

    removed identity, Core Domain Classes are now POCO, added EntityFramework to Core with dbcontext

--- Collection.Core/Domain/User.cs
+++ Collection.Core/Domain/User.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
-namespace Collection.Core.Domain
+﻿namespace Collection.Core.Domain
 {
-    public class User : IdentityUser
+    public class User
     {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
---
+++ Collection.Infrastructure/DAL/EntityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Collection.Core.Domain;
+
+namespace Collection.Infrastructure.DAL
+{
+    class EntityDBContext : DbContext, IDBContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
