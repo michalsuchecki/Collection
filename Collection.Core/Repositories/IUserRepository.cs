5a2f42cd13db726f9f28be05126b7a5ad99ef7bd
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-24 17:28:30 +0200

    Updated iuserrepository - using guid as id (Identity), get user by email

--- Collection.Core/Repositories/IUserRepository.cs
+++ Collection.Core/Repositories/IUserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Collection.Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,10 +7,11 @@ namespace Collection.Core.Repositories
 {
     public interface IUserRepository : IRepository
     {
-        Task<User> GetUserAsync(int id);
+        Task<User> GetUserAsync(Guid id);
+        Task<User> GetUserAsync(string email);
         Task<IEnumerable<User>> GetAllAsync();
         Task AddAsync(User user);
         Task UpdateAsync(User user);
-        Task RemoveAsync(int id);
+        Task RemoveAsync(Guid id);
     }
 }
