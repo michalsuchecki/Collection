02ff6cfc6346d58960af4d395ef0cff9774477d7
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-24 22:04:24 +0200

    dotnet ef cli, image primary key, first migration to new DB

--- Collection.Infrastructure/DAL/EntityDBContext.cs
+++ Collection.Infrastructure/DAL/EntityDBContext.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Collection.Core.Domain;
 
 namespace Collection.Infrastructure.DAL
 {
     class EntityDBContext : DbContext, IDBContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Collection_new;Trusted_Connection=True;MultipleActiveResultSets=true");
+            builder.EnableSensitiveDataLogging();
+
+            base.OnConfiguring(builder);
+        }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Producer> Producers { get; set; }
