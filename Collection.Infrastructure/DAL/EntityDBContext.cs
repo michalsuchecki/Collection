0dd67647a585d1758e44001be912bab2a69d1458
Michał Suchecki <michal_suchecki@outlook.com>
2017-05-25 13:23:38 +0200

    updated dbcontext interface

--- Collection.Infrastructure/DAL/EntityDBContext.cs
+++ Collection.Infrastructure/DAL/EntityDBContext.cs
@@ -1,24 +1,71 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Collection.Core.Domain;
+using System.Linq;
+using System;
 
 namespace Collection.Infrastructure.DAL
 {
     class EntityDBContext : DbContext, IDBContext
     {
+        private readonly string _connectionString;
+
+        public EntityDBContext() : this("Server=(localdb)\\MSSQLLocalDB;Database=Collection_new;Trusted_Connection=True;MultipleActiveResultSets=true")
+        {
+
+        }
+
+        public EntityDBContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Collection_new;Trusted_Connection=True;MultipleActiveResultSets=true");
+            builder.UseSqlServer(_connectionString);
             builder.EnableSensitiveDataLogging();
-
             base.OnConfiguring(builder);
         }
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Item> Items { get; set; }
-        public DbSet<Producer> Producers { get; set; }
-        public DbSet<Image> Images { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<User> Users { get; set; }
+        public IQueryable<User> GetUsers()
+        {
+            return Users;
+        }
+
+        public IQueryable<Producer> GetProducers()
+        {
+            return Producers;
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            return Categories;
+        }
+
+        public IQueryable<Image> GetImages()
+        {
+            return Images;
+        }
+
+        public IQueryable<Item> GetItems()
+        {
+            return Items;
+        }
+
+        public IQueryable<Post> GetPosts()
+        {
+            return Posts;
+        }
+
+        //public void UpdateEntity<T>(T entity) where T : class
+        //{
+        //    Update(entity);
+        //}
+
+        protected DbSet<Category> Categories { get; set; }
+        protected DbSet<Item> Items { get; set; }
+        protected DbSet<Producer> Producers { get; set; }
+        protected DbSet<Image> Images { get; set; }
+        protected DbSet<Post> Posts { get; set; }
+        protected DbSet<User> Users { get; set; }
     }
 }
--- Collection.Infrastructure/DAL/IDBContext.cs
+++ Collection.Infrastructure/DAL/IDBContext.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Collection.Core.Domain;
+using System.Linq;
 
 namespace Collection.Infrastructure.DAL
 {
     public interface IDBContext
     {
+        IQueryable<User> GetUsers();
+        IQueryable<Producer> GetProducers();
+        IQueryable<Category> GetCategories();
+        IQueryable<Image> GetImages();
+        IQueryable<Item> GetItems();
+        IQueryable<Post> GetPosts();
+
+        //void UpdateEntity<T>(T entity) where T : class;
     }
 }
