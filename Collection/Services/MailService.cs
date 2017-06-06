eb2aa2267d3798f78d01b9b66496b2060a23ef45
Michał Suchecki <michal_suchecki@outlook.com>
2017-06-06 17:54:36 +0200

    removed mail service for now

--- Collection/Services/MailService.cs
+++ Collection/Services/MailService.cs
@@ -24,54 +24,10 @@ namespace Collection.Services
 
         public MailService()
         {
-            //_from = new MailboxAddress("harukapl@gmail.com");
-            //_smtpPort = 587;
-            //_smtpAdress = "smtp.gmail.com";
-            //_smtpLogin = "harukapl@gmail.com";
-            //_smtpPassword = "dupamaryn123";
-
-            //_serviceAccountEmail = "mail-692@api-project-811987209687.iam.gserviceaccount.com";
-
-            //_certificate = new X509Certificate2(@"D:/test.p12", "notasecret", X509KeyStorageFlags.Exportable);
-
-            //credential = new ServiceAccountCredential(
-            //   new ServiceAccountCredential.Initializer(_serviceAccountEmail)
-            //   {
-            //       Scopes = new[] { "https://mail.google.com/" },
-            //       User = _smtpLogin
-            //   }.FromCertificate(_certificate));
         }
 
         public async Task SendEmail(string message, string topic, string name, string email)
         {
-            //bool result = await credential.RequestAccessTokenAsync(CancellationToken.None);
-
-            //if(true)
-            //{
-            //    var msg = new MimeMessage()
-            //    {
-            //        Subject = topic,
-            //        Body = new TextPart(TextFormat.Plain)
-            //        {
-            //            Text = $"{message}"
-            //        }
-            //    };
-
-            //    msg.From.Add(new MailboxAddress(name, email));
-            //    msg.To.Add(_from);
-
-            //    using (var client = new SmtpClient())
-            //    {
-            //        //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-            //        //client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-            //        await client.ConnectAsync(_smtpAdress, _smtpPort);
-            //        await client.AuthenticateAsync(_smtpLogin, credential.Token.AccessToken);
-
-            //        await client.SendAsync(msg);
-            //        await client.DisconnectAsync(true);
-            //    }
-            //}
         }
     }
 }
