5c21e95f999b3039b541a281e65073f3fd327211
Michał Suchecki <michal_suchecki@outlook.com>
2017-06-14 23:41:37 +0200

    fixed mail warning, updated html/css code

--- Collection/Services/MailService.cs
+++ Collection/Services/MailService.cs
@@ -13,14 +13,14 @@ namespace Collection.Services
     public class MailService : IMailService
     {
         // TODO: From config
-        private readonly MailboxAddress _from;
-        private readonly int _smtpPort;
-        private readonly string _smtpAdress;
-        private readonly string _smtpLogin;
-        private readonly string _smtpPassword;
-        private readonly X509Certificate2 _certificate;
-        private readonly string _serviceAccountEmail;
-        private readonly ServiceAccountCredential credential;
+        //private readonly MailboxAddress _from;
+        //private readonly int _smtpPort;
+        //private readonly string _smtpAdress;
+        //private readonly string _smtpLogin;
+        //private readonly string _smtpPassword;
+        //private readonly X509Certificate2 _certificate;
+        //private readonly string _serviceAccountEmail;
+        //private readonly ServiceAccountCredential credential;
 
         public MailService()
         {
@@ -28,6 +28,7 @@ namespace Collection.Services
 
         public async Task SendEmail(string message, string topic, string name, string email)
         {
+            await Task.CompletedTask;
         }
     }
 }
