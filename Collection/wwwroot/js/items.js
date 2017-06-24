a238d0f4586de44cdb0942814c5b4e174ad54443
Michał Suchecki <michal_suchecki@outlook.com>
2017-06-24 13:58:56 +0200

    - image preview when adding item

---
+++ Collection/wwwroot/js/items.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var imageContainer = $('#item-gallery');
+
+    $("#Images").on('change', function (e) {
+        $.each(e.target.files, function (index, file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                imageContainer.append(image_template(e.target.result));
+            }
+            reader.readAsDataURL(file);
+        });
+
+        $('.thumb').remove();
+        fill_gallery();
+    });
+
+    function image_template(image) {
+        return '<div class="col-md-3 thumb">' +
+                    '<div class="thumbnail">' +
+                        '<div class="img-box">' +
+                            '<img src="' + image + '" class="img-responsive" />' +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
+    };
+});
