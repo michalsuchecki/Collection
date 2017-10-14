2473ca1f846a4c0016c9b981bfd0f4149220d8bd
Michał Suchecki <michal_suchecki@outlook.com>
2017-10-14 19:27:09 +0200

    Api, Infrastructure, Core migrated from netcore 1.1 to netcore 2.0

--- Collection.Api/Program.cs
+++ Collection.Api/Program.cs
@@ -1,27 +1,24 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using Microsoft.AspNetCore;
 
 namespace Collection.Api
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider(cfg =>
+                {
+                    cfg.ValidateScopes = false;
+                })
+                .Build();
     }
 }
