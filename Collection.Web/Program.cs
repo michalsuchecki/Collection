47afe4c870eb1a76652e26db9e71be9a02c3c04e
Abigail <Michal_Suchecki@outlook.com>
2018-12-28 21:56:36 +0100

    added Collection.Web project + basic settings

--- Collection.Web/Program.cs
+++ Collection.Web/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+
 
 namespace Collection.Web
 {
@@ -17,9 +13,17 @@ namespace Collection.Web
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseStartup<Startup>()
+                          .ConfigureLogging(l =>
+                          {
+                              l.ClearProviders();
+                              l.SetMinimumLevel(LogLevel.Trace);
+                          })
+                          .UseNLog()
+                          .Build();
+        }
     }
 }
